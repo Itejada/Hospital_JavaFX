1926cd38f689bb2f8486c3d84f29bb8b5b9623dd
Itejada <ismael.elpuig@gmail.com>
2019-03-01 20:00:32 +0100

    ss

--- src/model/ControllerFilter.java
+++ src/model/ControllerFilter.java
@@ -1,44 +1,44 @@
-package model;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
-import javafx.scene.input.KeyEvent;
-
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.stream.Collectors;
-
-public class ControllerFilter implements Initializable {
-
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
-
-
-    public void btnCerca(ActionEvent event) {
-        List<Pacient> pacients = p.stream()
-                .filter(pacient -> pacient.getDNI().equals(txtDNI.getText()))
-                .collect(Collectors.toList());
-        if(txtDNI.getText().equals("")) {
-            updateTable(p);
-        }else updateTable(pacients);
-    }
-
-    private void updateTable(List<Pacient> pacients) {
-        data.clear();
-        data.addAll(pacients);
-        tablePacients.setItems(data);
-    }
-
-    public void changeText(KeyEvent keyEvent) {
-        data.clear();
-        List<Pacient> pacients = p.stream()
-                .filter(pacient -> pacient.getNom().contains(txtNom.getText()))
-                .filter((pacient -> pacient.getCognoms().contains(txtCognoms.getText())))
-                .collect(Collectors.toList());
-        data.addAll(pacients);
-
-    }
+//package model;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.Initializable;
+//import javafx.scene.input.KeyEvent;
+//
+//import java.net.URL;
+//import java.util.List;
+//import java.util.ResourceBundle;
+//import java.util.stream.Collectors;
+//
+//public class ControllerFilter implements Initializable {
+//
+//
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//
+//    }
+//
+//
+//    public void btnCerca(ActionEvent event) {
+//        List<Pacient> pacients = p.stream()
+//                .filter(pacient -> pacient.getDNI().equals(txtDNI.getText()))
+//                .collect(Collectors.toList());
+//        if(txtDNI.getText().equals("")) {
+//            updateTable(p);
+//        }else updateTable(pacients);
+//    }
+//
+//    private void updateTable(List<Pacient> pacients) {
+//        data.clear();
+//        data.addAll(pacients);
+//        tablePacients.setItems(data);
+//    }
+//
+//    public void changeText(KeyEvent keyEvent) {
+//        data.clear();
+//        List<Pacient> pacients = p.stream()
+//                .filter(pacient -> pacient.getNom().contains(txtNom.getText()))
+//                .filter((pacient -> pacient.getCognoms().contains(txtCognoms.getText())))
+//                .collect(Collectors.toList());
+//        data.addAll(pacients);
+//
+//    }
